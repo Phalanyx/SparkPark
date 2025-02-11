8697b26a62453d7bfe12af8dc8ed7b5a9e3a78a1
teddio496 <teddio496@gmail.com>
2025-02-11 15:41:54 -0500

    feat: added markers of greenp parking spots

--- frontend-mobile/app/index.tsx
+++ frontend-mobile/app/index.tsx
@@ -4,28 +4,11 @@ import Map from "./components/map";
 
 
 export default function Index() {
-  const [datas, setDatas] = useState<any>([]);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch('http://localhost:3000/api', { method: "GET" });
-        const result = await response.json();
-        setDatas(result);
-      } catch (error) {
-        setDatas([]);
-        console.error('There was an error making the request!', error);
-      }
-    };
-    fetchData();
-  }, []);
 
   return (
     <View style={{flex: 1}}>
       <Map />
-      <Text className="text-lg">The following are the data of parking spots in Toronto retrieved from our MongoDB database, through our node + express server.</Text>
-      <Text> {JSON.stringify(datas)}</Text>
-
     </View>
   );
 }

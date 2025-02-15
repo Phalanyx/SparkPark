eaef4889b48c11b21c4609e354d091c1a91f77fb
Albert Huynh <itsalbert.huynh@mail.utoronto.ca>
2025-02-14 20:19:49 -0500

    added google social auth

--- frontend-mobile/app/login/index.tsx
+++ frontend-mobile/app/login/index.tsx
@@ -1,8 +1,46 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Button, KeyboardAvoidingView, Text, TextInput, View } from 'react-native';
 import auth from '@react-native-firebase/auth';
+import {
+    GoogleSignin,
+    GoogleSigninButton,
+    statusCodes,
+  } from '@react-native-google-signin/google-signin';
+
 
 export default function Login() {
+
+    GoogleSignin.configure({
+        webClientId: '1053130768001-h8il86fb016cgres1om7h1jn1f1pi7kb.apps.googleusercontent.com',
+        });
+
+
+    const signIn = async () => {
+  // Check if your device supports Google Play
+    await GoogleSignin.hasPlayServices({ showPlayServicesUpdateDialog: true });
+    // Get the users ID token
+    const signInResult = await GoogleSignin.signIn();
+    console.log(signInResult);
+        
+    if (!signInResult.data) {
+        return;
+    }
+
+    
+
+    // Create a Google credential with the token
+    const googleCredential = auth.GoogleAuthProvider.credential(signInResult.data.idToken);
+
+    // Sign-in the user with the credential
+    return auth().signInWithCredential(googleCredential);
+        }
+
+    function logOut() {
+        auth()
+            .signOut()
+            .then(() => console.log('User signed out!'));
+    }  
+
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
@@ -45,6 +83,7 @@ export default function Login() {
 
     return (
         <View>
+
             <Text>Email</Text>
             <KeyboardAvoidingView behavior='padding'>
                 <TextInput
@@ -60,6 +99,8 @@ export default function Login() {
                 />
                 <Button title="Login" onPress={handleLogin} />
                 <Button title="Register" onPress={handleRegister} />
+                <GoogleSigninButton onPress={signIn} />
+                <Button title="Log out" onPress={logOut} />
             </KeyboardAvoidingView>
         </View>
     )
